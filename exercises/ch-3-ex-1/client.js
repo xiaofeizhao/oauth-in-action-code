1a5b6dc7d5f70410b76134d6fb9b46164c6fb7eb
Michael Zhao <michaelxzhao@icloud.com>
2019-09-07 17:51:43 -0700

    3.1 complete

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,8 +27,8 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -48,7 +48,15 @@ app.get('/authorize', function(req, res){
 	/*
 	 * Send the user to the authorization server
 	 */
-	
+	state = randomstring.generate();
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0],
+		state: state
+	});
+
+	res.redirect(authorizeUrl);
 });
 
 app.get('/callback', function(req, res){
@@ -56,7 +64,31 @@ app.get('/callback', function(req, res){
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
-	
+	if(req.query.state != state){
+		res.render('error', {error: 'State value did not match'});
+		return;
+	}
+
+	var code = req.query.code;
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+	var tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	});
+
+	var body = JSON.parse(tokRes.getBody());
+
+	access_token = body.access_token;
+
+	res.render('index', {access_token: access_token, scope: scope});
 });
 
 app.get('/fetch_resource', function(req, res) {
@@ -64,7 +96,25 @@ app.get('/fetch_resource', function(req, res) {
 	/*
 	 * Use the access token to call the resource server
 	 */
-	
+	if(!access_token){
+		res.redirect('/authorize');
+		return;
+	}
+
+	var headers = {
+		'Authorization': 'Bearer ' + access_token
+	};
+
+	var resource = request('POST', protectedResource, {headers: headers});
+
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody());
+		res.render('data', {resource: body});
+		return;
+	}else{
+		res.render('error', {error: 'Server returned response code: ' + response.statusCode});
+		return;
+	}
 });
 
 var buildUrl = function(base, options, hash) {
